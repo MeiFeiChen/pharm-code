a9a3e4e6f5f3ac07c35acac63a811f93240f3a36
ameliachen <meifeiccc@gmail.com>
2023-11-21 20:32:17 +0800

    Modified: import route in client

--- client/src/pages/Problem.jsx
+++ client/src/pages/Problem.jsx
@@ -1,6 +1,6 @@
 import { Link, useParams} from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import TopBar from '../components/Topbar'
+import TopBar from '../components/TopBar'
 import WorkSpace from '../components/Workspace/WorkSpace'
 
 
